0d66a075cac80c0af5bb6c9e2b03430832e108d3
Andres-Vasquezunige <116440246+Andres-Vasquezunige@users.noreply.github.com>
2022-11-09 21:34:21 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
+// Andres Misael Vasquez Aizacayo
 #include <iostream>
 using namespace std;
-
+#include <math.h>
 int main(){
-//Qui vanno definite le variabili
-
+float a=0,b=0,c=0;//Qui vanno definite le variabili
 cin >> a >> b >> c;
-
-//Qui va scritto il codice per eseguire i calcoli richiesti
-
-
+cout<<"Area del triangolo="<<(a*b)/2<<endl;//Qui va scritto il codice per eseguire i calcoli richiesti
+cout<<"Area del quadrato="<<pow(a,2)<<endl;
+cout<<"Area del rettangolo="<<(a*b)<<endl;
+cout<<"Area del trapezio="<<(((a+b)*c)/2)<<endl;
 }
