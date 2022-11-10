4ca0e0310154f6ab1cb8a4e89a173c171d3746da
Andres-Vasquezunige <116440246+Andres-Vasquezunige@users.noreply.github.com>
2022-11-10 22:32:37 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(){
 float a=0,b=0,c=0;//Qui vanno definite le variabili
 cin >> a >> b >> c;
 cout<<"Area del triangolo="<<(a*b)/2<<endl;//Qui va scritto il codice per eseguire i calcoli richiesti
-cout<<"Area del quadrato="<<pow(a*a)<<endl;
+cout<<"Area del quadrato="<<(a*a)<<endl;
 cout<<"Area del rettangolo="<<(a*b)<<endl;
 cout<<"Area del trapezio="<<(((a+b)*c)/2)<<endl;
 }
